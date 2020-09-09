2f3178e521c654f31bb4b63b9df93a39c7613997
Tobi Tselios <tobi.tselios@gmail.com>
2020-09-09 22:55:36 +0100

    MessageKit Message model
    Co-authored-by: Tobi tobi.tselios@gmail.com
    Co-authored-by: Hunter hunter.oppel@gmail.com

--- labs-ios-starter/Model/Message.swift
+++ labs-ios-starter/Model/Message.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import MessageKit
+
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
