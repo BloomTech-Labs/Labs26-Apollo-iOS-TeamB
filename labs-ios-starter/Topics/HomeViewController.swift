c229301b6e19635a60a33a785fa7b4d72f30c747
Tobi Tselios <tobi.tselios@gmail.com>
2020-09-10 23:21:59 +0100

    Created view controllers for the Topics storyboard
    Co-authored-by: Tobi tobi.tselios@gmail.com
    Co-authored-by: Hunter hunter.oppel@gmail.com

---
+++ labs-ios-starter/Topics/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  labs-ios-starter
+//
+//  Created by Tobi Kuyoro on 10/09/2020.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var userAvatar: UIButton!
+    @IBOutlet var codeTextField: UITextField!
+    @IBOutlet var joinCodeButton: UIButton!
+    @IBOutlet var createTopicButton: UIButton!
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        styleButtons()
+    }
+
+    // MARK: - Methods
+
+    private func styleButtons() {
+        joinCodeButton.layer.cornerRadius = 10
+        createTopicButton.layer.cornerRadius = 10
+    }
+
+    private func setTitle() {
+    }
+
+    // MARK: - Actions
+
+    @IBAction func joinCodeButtonTapped(_ sender: Any) {
+    }
+}
