0302f3d9039985836eccfa2e0e2ed26f159a1c97
Tobi Tselios <tobi.tselios@gmail.com>
2020-09-15 23:20:49 +0100

    Created a TopicResults Model and updated Topic Model, based on current supplied model from the backend
    Co-authored-by: Tobi tobi.tselios@gmail.com
    Co-authored-by: Hunter hunter.oppel@gmail.com

--- labs-ios-starter/Model/Topic.swift
+++ labs-ios-starter/Model/Topic.swift
@@ -9,34 +9,73 @@
 import Foundation
 
 class Topic: Codable {
-    let id: Int?
-    let ownerID: Int?
+    let topicId: Int?
+    let userId: Int?
     let title: String
     let surveys: [Int]?
-    let dateCreated: Date?
-    let lastModified: Date?
-    let frequency: String
-    let contextType: Context
-    let leaderQuestions: [Question]
-    let memberQuestions: [Question]
+    let users: [User]?
+    var dateCreated: Date? = nil
+    var lastModified: Date? = nil
+    var frequency: String? = nil
+    var contextType: Context? = nil
+    var leaderQuestions: [Question]? = nil
+    var memberQuestions: [Question]? = nil
 
-    init(id: Int?,
-         ownerID: Int?,
+    enum TopicKeys: String, CodingKey {
+        case topicId
+        case userId
+        case title
+        case surveys = "survey"
+        case users
+        case lastModified
+        case frequency
+        case contextType
+        case leaderQuestions
+        case memberQuestions
+        case owner
+        case surveyId
+    }
+
+    required init(from decoder: Decoder) throws {
+        let topicContainer = try decoder.container(keyedBy: TopicKeys.self)
+        let ownerContainer = try topicContainer.nestedContainer(keyedBy: TopicKeys.self, forKey: .owner)
+        var surveyContainer = try topicContainer.nestedUnkeyedContainer(forKey: .surveys)
+        let surveyIdContainer = try surveyContainer.nestedContainer(keyedBy: TopicKeys.self)
+
+        userId = try ownerContainer.decode(Int.self, forKey: .userId)
+        title = try topicContainer.decode(String.self, forKey: .title)
+        topicId = try topicContainer.decode(Int.self, forKey: .topicId)
+        users = try topicContainer.decode([User].self, forKey: .users)
+
+        var surveyIds: [Int] = []
+
+        while !surveyContainer.isAtEnd {
+            let surveyID = try surveyIdContainer.decode(Int.self, forKey: .surveyId)
+            surveyIds.append(surveyID)
+        }
+
+        surveys = surveyIds
+    }
+
+    init(topicId: Int?,
+         userId: Int?,
          title: String,
          surveys: [Int]?,
          dateCreated: Date?,
          lastModified: Date?,
-         frequency: String,
-         contextType: Context) {
-        self.id = id
-        self.ownerID = ownerID
+         frequency: String?,
+         contextType: Context?,
+         users: [User]?) {
+        self.topicId = topicId
+        self.userId = userId
         self.title = title
         self.surveys = surveys
         self.dateCreated = dateCreated
         self.lastModified = lastModified
         self.frequency = frequency
         self.contextType = contextType
-        self.leaderQuestions = contextType.leaderQuestions
-        self.memberQuestions = contextType.memberQuestions
+        self.leaderQuestions = contextType!.leaderQuestions
+        self.memberQuestions = contextType!.memberQuestions
+        self.users = users
     }
 }
