980ca1a0e90e8afe04cb386677bbee8c7d176f4d
Tobi Tselios <tobi.tselios@gmail.com>
2020-09-22 15:15:22 +0100

    Method to fetch all topics

---
+++ labs-ios-starter/Networking/APIController.swift
@@ -0,0 +1,42 @@
+
+//
+//  APIController.swift
+//  labs-ios-starter
+//
+//  Created by Tobi Kuyoro on 22/09/2020.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+class APIContoller {
+
+    private let baseURL = URL(string: "http://apollo-b-api.herokuapp.com")!
+
+    func fetchTopics(completion: @escaping (TopicResults?) -> Void) {
+        let requestURL = baseURL.appendingPathComponent("topics").appendingPathComponent("topics")
+        let request = URLRequest(url: requestURL)
+
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                NSLog("Error fetching topics: \(error)")
+                completion(nil)
+                return
+            }
+
+            guard let data = data else {
+                NSLog("No topic data for topics request")
+                completion(nil)
+                return
+            }
+
+            do {
+                let topics = try JSONDecoder().decode(TopicResults.self, from: data)
+                DispatchQueue.main.async { completion(topics) }
+            } catch {
+                NSLog("Error decoding topics data: \(error)")
+                completion(nil)
+            }
+        }.resume()
+    }
+}
