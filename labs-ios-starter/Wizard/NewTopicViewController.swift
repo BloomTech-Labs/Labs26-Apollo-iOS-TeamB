0e757eb31f5cb0bc12060097b2cea1284304343f
Tobi Tselios <tobi.tselios@gmail.com>
2020-09-29 21:43:28 +0100

    We fetched the contexts and displayed them on the table view
    Co-authored-by: tobikuyo tobi.tselios@gmail.com
    Co-authored-by: hoppel16 hun.oppen@gmail.com

--- labs-ios-starter/Wizard/NewTopicViewController.swift
+++ labs-ios-starter/Wizard/NewTopicViewController.swift
@@ -12,8 +12,37 @@ class NewTopicViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
 
+    var contexts: [Context] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("NEW TOPIC")
+
+        UserController.shared.fetchContexts { contexts in
+            if let contexts = contexts {
+                self.contexts = contexts.results
+
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+}
+
+extension NewTopicViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contexts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewTopicCell", for: indexPath)
+        cell.textLabel?.text = contexts[indexPath.row].description.capitalized
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
