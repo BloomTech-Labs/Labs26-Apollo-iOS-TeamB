f0294f227e2425f12d43fd97c42ea2fa3c4903a5
Tobi Tselios <tobi.tselios@gmail.com>
2020-09-30 19:10:23 +0100

    Initial attempt to create topic and get the join code from the created topic Co-authored-by: tobikuyo tobi.tselios@gmail.com Co-authored-by: hoppel16 hun.oppel@gmail.com

--- labs-ios-starter/Wizard/TopicCodeViewController.swift
+++ labs-ios-starter/Wizard/TopicCodeViewController.swift
@@ -11,18 +11,36 @@ import UIKit
 class TopicCodeViewController: UIViewController {
 
     @IBOutlet var completedButton: UIButton!
+    @IBOutlet var joinCodeButton: UIButton!
+
+    var joinCode: String?
+    var newTopic: Topic?
 
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         styleButton()
+        createNewTopic()
     }
 
     // MARK: - Actions
 
     private func styleButton() {
         completedButton.layer.cornerRadius = 10
+    }
+
+    private func createNewTopic() {
+        guard let newTopic = newTopic else { return }
+        UserController.shared.createTopic(newTopic) { topic in
+            if let topic = topic,
+                let joinCode = topic.joincode {
+
+                DispatchQueue.main.async {
+                    self.joinCodeButton.setTitle(joinCode, for: .normal)
+                }
+            }
+        }
     }
 
     @IBAction func codeCopyTapped(_ sender: Any) {
