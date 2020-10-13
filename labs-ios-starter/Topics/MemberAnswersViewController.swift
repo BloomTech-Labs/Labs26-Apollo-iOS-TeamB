66f88d6daf42732619bd7cdba5c3f39531b43b6d
Tobi Tselios <tobi.tselios@gmail.com>
2020-10-13 01:53:32 +0100

    Created MemberAnswersViewController and made changes to the project

--- labs-ios-starter/Topics/MemberAnswersViewController.swift
+++ labs-ios-starter/Topics/MemberAnswersViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MemberQuestionsViewController.swift
+//  MemberAnswersViewController.swift
 //  labs-ios-starter
 //
 //  Created by Tobi Kuyoro on 12/10/2020.
@@ -8,25 +8,74 @@
 
 import UIKit
 
-class MemberQuestionsViewController: UIViewController {
+class MemberAnswersViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
 
     var surveyId: Int?
+    var questions: [Question] = []
+    var responses: [Question] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        fetchQuestions()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBAction func responseButtonTapped(_ sender: Any) {
+        getResponses()
+        UserController.shared.answerSurveyRequest(for: responses) { error in
+            if let error = error {
+                NSLog("Error posting responses: \(error)")
+            }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            DispatchQueue.main.async {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
-    */
 
+    private func getResponses() {
+        guard let cells = tableView.visibleCells as? [MemberAnswersTableViewCell] else { return }
+
+        for cell in cells {
+            guard
+                let question = cell.question,
+                let questionId = question.questionid,
+                !cell.answerTextView.text.isEmpty,
+                let body = cell.answerTextView.text else { return }
+            let response = Question(questionId: questionId, body: body)
+            responses.append(response)
+        }
+    }
+
+    private func fetchQuestions() {
+        guard let surveyId = surveyId else { return }
+        UserController.shared.fetchSpecificSurvey(with: surveyId) { results in
+            if let results = results,
+                let questions = results.questions {
+                for question in questions {
+                    if question.leader == false {
+                        self.questions.append(question)
+                    }
+                }
+
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+}
+
+extension MemberAnswersViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        questions.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MembersCell", for: indexPath) as? MemberAnswersTableViewCell else { return UITableViewCell() }
+
+        cell.question = questions[indexPath.row]
+        return cell
+    }
 }
