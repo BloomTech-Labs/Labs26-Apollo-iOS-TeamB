22879121346a660e8e358dae5574b979cb70e10c
Tobi Tselios <tobi.tselios@gmail.com>
2020-10-14 16:05:08 +0100

    Changes to the look of the question label and answer text view for context and member questions

--- labs-ios-starter/Topics/AnswersTableViewCell.swift
+++ labs-ios-starter/Topics/AnswersTableViewCell.swift
@@ -21,7 +21,10 @@ class AnswersTableViewCell: UITableViewCell {
 
     private func updateViews() {
         guard let question = question else { return }
-        questionLabel.text = question.body
+        questionLabel.text = "   \(question.body ?? "")"
+        questionLabel.layer.cornerRadius = 5
+        questionLabel.layer.borderWidth = 1
+        questionLabel.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
         answerTextView.text = question.answers?.first?.body
     }
 }
