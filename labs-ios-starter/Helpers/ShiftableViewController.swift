456c86a4fee952b8e53f956cabda3fd6ac558c31
Tobi Tselios <tobi.tselios@gmail.com>
2020-10-16 00:15:22 +0100

    Created ShiftableViewController to solve keyboard bug

--- labs-ios-starter/Helpers/ShiftableViewController.swift
+++ labs-ios-starter/Helpers/ShiftableViewController.swift
@@ -8,33 +8,102 @@
 
 import UIKit
 
-class ShiftableViewController: UIViewController {
+class ShiftableViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIGestureRecognizerDelegate {
+
+    var currentYShiftForKeyboard: CGFloat = 0
+    var textFieldBeingEdited: UITextField?
+    var textViewBeingEdited: UITextView?
+    var keyboardDismissTapGestureRecognizer: UITapGestureRecognizer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupKeyboardDismissTapGestureRecognizer()
 
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
+                                               selector: #selector(keyboardWillShow(notification:)),
                                                name: UIResponder.keyboardWillShowNotification,
                                                object: nil)
+
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
+                                               selector: #selector(keyboardWillHide(notification:)),
                                                name: UIResponder.keyboardWillHideNotification,
                                                object: nil)
-
     }
 
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+    @objc func stopEditingTextInput() {
+        if let textField = self.textFieldBeingEdited {
+            textField.resignFirstResponder()
+            self.textFieldBeingEdited = nil
+            self.textViewBeingEdited = nil
+        } else if let textView = self.textViewBeingEdited {
+            textView.resignFirstResponder()
+            self.textFieldBeingEdited = nil
+            self.textViewBeingEdited = nil
+        }
+
+        guard keyboardDismissTapGestureRecognizer.isEnabled else { return }
+        keyboardDismissTapGestureRecognizer.isEnabled = false
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textFieldBeingEdited = textField
+    }
+
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        textViewBeingEdited = textView
+        return true
+    }
+
+    @objc func keyboardWillShow(notification: Notification) {
+        keyboardDismissTapGestureRecognizer.isEnabled = true
+        var keyboardSize: CGRect = .zero
+
+        if let keyboardRect = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+            keyboardRect.height != 0 {
+            keyboardSize = keyboardRect
+        } else if let keyboardRect = notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? CGRect {
+            keyboardSize = keyboardRect
+        }
+
+        if let textField = textFieldBeingEdited {
             if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
+                let yShift = yShiftWhenKeyboardAppearsFor(textInput: textField, keyboardSize: keyboardSize, nextY: keyboardSize.height)
+                self.currentYShiftForKeyboard = yShift
+                self.view.frame.origin.y -= yShift
+            }
+
+        } else if let textView = textViewBeingEdited {
+            if self.view.frame.origin.y == 0 {
+                let yShift = yShiftWhenKeyboardAppearsFor(textInput: textView, keyboardSize: keyboardSize, nextY: keyboardSize.height)
+                self.currentYShiftForKeyboard = yShift
+                self.view.frame.origin.y -= yShift
             }
         }
     }
 
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
+    @objc func yShiftWhenKeyboardAppearsFor(textInput: UIView, keyboardSize: CGRect, nextY: CGFloat) -> CGFloat {
+        let textFieldOrigin = self.view.convert(textInput.frame, from: textInput.superview!).origin.y
+        let textFieldBottomY = textFieldOrigin + textInput.frame.size.height
+        let maximumY = self.view.frame.height - (keyboardSize.height + view.safeAreaInsets.bottom)
+        if textFieldBottomY > maximumY {
+            return textFieldBottomY - maximumY
+        } else {
+            return 0
         }
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y += currentYShiftForKeyboard
+        }
+        
+        stopEditingTextInput()
+    }
+
+    @objc func setupKeyboardDismissTapGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(stopEditingTextInput))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tapGestureRecognizer)
+        keyboardDismissTapGestureRecognizer = tapGestureRecognizer
     }
 }
